677fd6c418cff86b186d3df682ea45ac886d4c0f
boernick <nick.boer@hotmail.com>
2024-12-01 21:32:55 +0100

    update woring exec

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -29,25 +29,19 @@ void	exec_mini(t_shell *shell, t_execution *pipex)
 	while (pipex->index_cmd < pipex->n_cmds)
 	{
 		setup_redirections(pipex->cmd);
-		if (pipex->cmd->is_builtin == 1 && pipex->n_pipes == 0)
-		{
-			get_fd(pipex, pipex->cmd); //DUP2 to STDIN/OUT
-			run_builtin(do_builtin(pipex->cmd->argv), pipex->cmd->argv, shell);
-			reset_fds(pipex);
-			return ;
-		}
+		if (pipex->cmd->is_builtin == 1)
+			run_builtin(do_builtin(pipex->cmd->argv), pipex->cmd->argv, shell); //need to solve redirections for builtins
+		pids[i] = fork_child();
 		if (pids[i++] == 0)
 		{
 			get_fd(pipex, pipex->cmd); //DUP2 to STDIN/OUT
 			clean_pipes(pipex, pipex->cmd); //CLOSING FDS
-			if (pipex->cmd->is_builtin == 1)
-				run_builtin(do_builtin(pipex->cmd->argv), pipex->cmd->argv, shell);
-			else
-				run_ex(pipex->cmd, envlst_to_array(shell)); //RUN EX
+			run_ex(pipex->cmd, envlst_to_array(shell)); //RUN EX
 		}
 		else
 			update_exec(pipex);
 	}
+	// print_lst(shell->env_lst);
 	clean_pipes(pipex, pipex->cmd);
 	waitpids(pids, pipex->n_cmds);
 }
