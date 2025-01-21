b3432ae36121a53a029a3d0ad81003e29ab3986b
Nick Boer <nboer@c3a3c5.42berlin.de>
2025-01-21 16:06:58 +0100

    update

--- srcs/main/run_mini.c
+++ srcs/main/run_mini.c
@@ -6,20 +6,20 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:51:09 by nboer             #+#    #+#             */
-/*   Updated: 2025/01/21 16:00:15 by nboer            ###   ########.fr       */
+/*   Updated: 2025/01/21 16:05:54 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void run_mini(t_parse *parse, t_shell *shell, t_execution *pipex)
+void	run_mini(t_parse *parse, t_shell *shell, t_execution *pipex)
 {
-	parse_tokens(&parse, &shell);
+	parse_tokens(parse, shell);
 	//print_command_stack(pipex.cmd); // REMOVE
 	//check_temp_files(parse.cmd); // REMOVE
 	if (parse->valid_input)
 	{
 		pipex->cmd = parse->cmd;
-		exec_mini(&shell, &pipex);
+		exec_mini(shell, pipex);
 	}
 }
--- srcs/tokenize/handle_struct.c
+++ srcs/tokenize/handle_struct.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_struct.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 16:06:42 by nboer             #+#    #+#             */
+/*   Updated: 2025/01/21 16:06:42 by nboer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-// Initializes the fields of the t_data struct for starting a new shell instance.
+// Initializes the fields of the t_data struct for starting new shell instance.
 void	struct_init(t_parse *data, t_shell *shell)
 {
 	shell->exit = 0;
@@ -14,7 +26,7 @@ void	struct_init(t_parse *data, t_shell *shell)
 	data->in_single_quote = 0;
 }
 
-// Resets specific fields in t_data struct to prepare for processing a new command.
+// Resets specific fields in t_data struct to prepare processing a new command.
 void	reset_parse(t_parse *data)
 {
 	data->head = NULL;
